2ecb2611eb9a3ec2712b76a3785da173d0551dd1
Brian Dutz and Seth Kendall <brian@>
2016-10-10 14:02:37 -0700

    finish gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
+var utilities = require('gulp-util');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var del = require('del');
+
+var buildProduction = utilities.env.production;
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
 
 gulp.task('jsBrowserify', function() {
   return browserify({ entries: ['./js/journal-interface.js'] })
@@ -14,4 +23,18 @@ gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
